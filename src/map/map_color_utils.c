1324071bbe5b4ca1fa038e4dfa8b1c30c25b52c6
TRSTN4 <trstnvb4@gmail.com>
2024-05-22 13:19:20 +0200

    Added imrpoved rgb error handling.

--- src/map/map_color_utils.c
+++ src/map/map_color_utils.c
@@ -6,7 +6,7 @@
 /*   By: trstn4 <trstn4@student.codam.nl>             +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/04/03 14:50:42 by trstn4        #+#    #+#                 */
-/*   Updated: 2024/05/16 21:16:30 by trstn4        ########   odam.nl         */
+/*   Updated: 2024/05/22 13:15:13 by trstn4        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int	cub_rgb_to_hex(int r, int g, int b)
 	return (r << 24 | g << 16 | b << 8 | 255 << 0);
 }
 
-void	check_digits_rgb(char **rgb_parts)
+int	check_digits_rgb(char **rgb_parts)
 {
 	int	i;
 	int	j;
@@ -29,15 +29,20 @@ void	check_digits_rgb(char **rgb_parts)
 		while (rgb_parts[i][j] != '\0')
 		{
 			if (!ft_isdigit(rgb_parts[i][j]) && rgb_parts[i][j] != ' ')
-				cub_error(1, "Error: Invalid RGB value\n");
+			{
+				free(rgb_parts);
+				cub_error(1, "Error\nInvalid RGB value\n");
+			}
 			j++;
 		}
 		if (ft_atoi(rgb_parts[i]) < 0 || ft_atoi(rgb_parts[i]) > 255)
-			cub_error(1, "Error: Invalid RGB value\n");
+		{
+			free(rgb_parts);
+			cub_error(1, "Error\nInvalid RGB value\n");
+		}
 		i++;
-		if (i > 3)
-			cub_error(1, "Error: Invalid RGB value\n");
 	}
+	return (i);
 }
 
 unsigned int	cub_parse_rgb_string_to_hex(char *rgb_string)
@@ -50,7 +55,11 @@ unsigned int	cub_parse_rgb_string_to_hex(char *rgb_string)
 	rgb_parts = ft_split(rgb_string, ',');
 	if (!rgb_parts)
 		return (0);
-	check_digits_rgb(rgb_parts);
+	if (check_digits_rgb(rgb_parts) != 3)
+	{
+		free(rgb_parts);
+		cub_error(1, "Error\nInvalid RGB value\n");
+	}
 	hex_color = cub_rgb_to_hex(ft_atoi(rgb_parts[0]), ft_atoi(rgb_parts[1]),
 			ft_atoi(rgb_parts[2]));
 	while (rgb_parts[i] != NULL)
