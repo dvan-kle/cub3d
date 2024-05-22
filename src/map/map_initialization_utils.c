706565b9383d4502b0a7ef2a16cace3cae334ff0
TRSTN4 <trstnvb4@gmail.com>
2024-05-22 12:59:20 +0200

    Added new maps + more error handling.

--- src/map/map_initialization_utils.c
+++ src/map/map_initialization_utils.c
@@ -6,7 +6,7 @@
 /*   By: trstn4 <trstn4@student.codam.nl>             +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/04/03 14:51:31 by trstn4        #+#    #+#                 */
-/*   Updated: 2024/05/17 15:48:44 by trstn4        ########   odam.nl         */
+/*   Updated: 2024/05/22 12:55:40 by trstn4        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,10 +19,10 @@ t_map	*cub_init_map_and_open_file(char *file, int *fd, int *fd2)
 	map = ft_calloc(1, sizeof(t_map));
 	*fd = open(file, O_RDONLY);
 	if (!map || *fd < 0)
-		cub_error(1, "Error: Initialization or File Opening Failed.\n");
+		cub_error(1, "Error\nFile opening failed.\n");
 	*fd2 = open(file, O_RDONLY);
 	if (!map || *fd2 < 0)
-		cub_error(1, "Error: Initialization or File Opening Failed.\n");
+		cub_error(1, "Error\nFile opening failed.\n");
 	return (map);
 }
 
@@ -39,9 +39,17 @@ void	free_map_ids(t_map *map)
 void	cub_finalize_map(t_map *map)
 {
 	map->north_texture = mlx_load_png(map->id_no);
+	if (!map->north_texture)
+		cub_error(1, "Error\nNorth texture loading failed.\n");
 	map->south_texture = mlx_load_png(map->id_so);
+	if (!map->south_texture)
+		cub_error(1, "Error\nSouth texture loading failed.\n");
 	map->west_texture = mlx_load_png(map->id_we);
+	if (!map->west_texture)
+		cub_error(1, "Error\nWest texture loading failed.\n");
 	map->east_texture = mlx_load_png(map->id_ea);
+	if (!map->east_texture)
+		cub_error(1, "Error\nEast texture loading failed.\n");
 	map->color_ceiling = cub_parse_rgb_string_to_hex(map->id_c);
 	map->color_floor = cub_parse_rgb_string_to_hex(map->id_f);
 	free_map_ids(map);
